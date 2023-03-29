8986b25f39a529f027f9d03c8618ccbb86f436ab
tws <thdxodns@naver.com>
2023-03-29 09:46:57 +0900

    0329

--- inc/dbcon - 복사본.php
+++ inc/dbcon - 복사본.php
@@ -2,4 +2,5 @@
 //$dbcon = mysqli_connect("mysql","root","", "front") or die("접속 실패");
 $dbcon = mysqli_connect("localhost","tws1","Tt8474204!", "tws1") or die("접속 실패");
 mysqli_set_charset($dbcon, "utf8");
+
 ?>
